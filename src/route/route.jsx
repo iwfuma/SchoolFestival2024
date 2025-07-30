beb7dc1b1bc061d383aa3e27c7d870ef621141bf
Iwami Fuma <sado24230023@nsgcl.jp>
2025-07-30 21:01:36 +0900

    refs #2:ホームページ作成

--- src/route/route.jsx
+++ src/route/route.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../views/Home";
 import OneClickHome from "../views/oneClick/oneClickHome";
+import Claim from "../views/oneClick/Claim";
 import PhishingMail from "../views/phissing/phishingMail";
 
 const AppRoutes = () => {
@@ -9,6 +10,7 @@ const AppRoutes = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/oneclick" element={<OneClickHome />} />
+      <Route path="/oneclick/claim" element={<Claim/>}/>
       <Route path="/phishing" element={<PhishingMail />} />
     </Routes>
   );
---
+++ src/views/oneClick/Claim/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Claim = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h2>請求 画面</h2>
+    </div>
+  );
+};
+
+export default Claim;
