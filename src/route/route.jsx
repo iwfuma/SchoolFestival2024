d779dc84c2ce8a75b8b610949c32369c1e449949
Iwami Fuma <sado24230023@nsgcl.jp>
2025-07-31 19:21:14 +0900

    refs #2:route修正

--- src/route/route.jsx
+++ src/route/route.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../views/Home";
-import OneClickHome from "../views/oneClick/oneClickHome";
+import OneClickHome from "../views/oneClick/Home";
 import Claim from "../views/oneClick/Claim";
 import PhishingMail from "../views/phissing/phishingMail";
 
@@ -9,6 +9,7 @@ const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
       <Route path="/oneclick" element={<OneClickHome />} />
       <Route path="/oneclick/claim" element={<Claim/>}/>
       <Route path="/phishing" element={<PhishingMail />} />
